eff26cba121dee203e79dced53e360082a7b1876
martalexa <alexandramartinpro@gmail.com>
2018-01-08 14:36:37 +0100

    se deconnecter

---
+++ coop/src/components/ConversationsListe.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="seDeconnecter">Se deconnecter</button>
+</template>
+
+<script>
+export default {
+  name: 'ConversationsListe',
+  data () {
+    return {
+    }
+	},
+	methods:{
+		  seDeconnecter(){
+        window.axios.post('members/signout?token="$this.store"',{}).then((response) => {
+
+          alert(reponse.data.fullname);
+
+        }).catch ((error) => {
+          alert(error.response.data.error.join(""));
+        })
+
+
+      }
+		}
+}
+
+</script>
+
+
+<style scoped>
+</style>
